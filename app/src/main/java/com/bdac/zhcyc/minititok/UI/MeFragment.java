fb700d72d005d6cc9c7d38d7c4a762283c108b63
wz <13520481028zhc@gmail.com>
2019-01-28 18:40:38 +0800

    Add info change func

--- app/src/main/java/com/bdac/zhcyc/minititok/UI/MeFragment.java
+++ app/src/main/java/com/bdac/zhcyc/minititok/UI/MeFragment.java
@@ -2,17 +2,26 @@ package com.bdac.zhcyc.minititok.UI;
 
 import android.app.Activity;
 import android.app.ActivityOptions;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bdac.zhcyc.minititok.R;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.ActivityOptionsCompat;
 import androidx.fragment.app.Fragment;
@@ -25,15 +34,70 @@ import static com.bdac.zhcyc.minititok.Utilities.DatabaseUtils.loadItemsFromData
 
 public class MeFragment extends Fragment implements ThumbsAdapter.OnThumbClickListener {
 
+    public final static String TAG = "MeFragment";
+
     private RecyclerView recyclerView;
     private ThumbsAdapter adapter;
     private GridLayoutManager layoutManager;
     private SwipeRefreshLayout swipeRefreshLayout;
+    private CoordinatorLayout personalInfoLayout;
+    private AlertDialog alertDialog;
+    private EditText userNameEditText;
+    private EditText studentIdEditText;
+    private SharedPreferences sharedPreferences;
+    private static final String PACKAGE_NAME = "com.bdac.zhcyc.minitiktok";
+    private TextView nameTextView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        sharedPreferences = getActivity().getSharedPreferences(PACKAGE_NAME, Context.MODE_PRIVATE);
         View view = inflater.inflate(R.layout.fragment_me, container, false);
+        View diagView = inflater.inflate(R.layout.input_layout, (ViewGroup) getActivity().findViewById(R.id.input_layout));
+
+        nameTextView = view.findViewById(R.id.textView_name);
+        studentIdEditText = diagView.findViewById(R.id.et_student_id);
+        userNameEditText = diagView.findViewById(R.id.et_user_name);
+        personalInfoLayout = view.findViewById(R.id.personal_info_layout);
+        personalInfoLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+                if (alertDialog != null && alertDialog.isShowing()) {
+                    alertDialog.dismiss();
+                }
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle(R.string.input_info);
+                if (diagView.getParent() != null) {
+                    ((ViewGroup)diagView.getParent()).removeView(diagView);
+                }
+                builder.setView(diagView);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //TODO
+                        if (userNameEditText.getText().toString() == "" || studentIdEditText.getText().toString() == "") {
+                            Toast.makeText(getContext(), "Wrong Input", Toast.LENGTH_SHORT).show();
+                        } else {
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString(getString(R.string.sp_student_id), studentIdEditText.getText().toString());
+                            editor.putString(getString(R.string.sp_user_name), userNameEditText.getText().toString());
+                            editor.apply();
+                            Log.d(TAG, "onClick: Changed");
+                            refreshName();
+                        }
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                alertDialog = builder.show();
+            }
+        });
+
         recyclerView = view.findViewById(R.id.rv_thumbs);
         adapter = new ThumbsAdapter();
         layoutManager = new GridLayoutManager(getContext(), 3);
@@ -72,7 +136,13 @@ public class MeFragment extends Fragment implements ThumbsAdapter.OnThumbClickLi
 
         adapter.setOnThumbClickListener(this);
 
+        refreshName();
         return view;
+    }
+
+    private void refreshName() {
+        Log.d(TAG, "refreshName: " + sharedPreferences.getString(getString(R.string.sp_user_name), "zhcyc"));
+        nameTextView.setText(sharedPreferences.getString(getString(R.string.sp_user_name), "zhcyc"));
     }
 
     public void refreshData () {
